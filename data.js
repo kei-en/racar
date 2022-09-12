265e898b527aff7e2e417008e0357d05380eed3a
Kei En <njuguna0karanja@gmail.com>
2022-09-12 17:49:52 +0300

    explore nearby

---
+++ data.js
@@ -0,0 +1,33 @@
+import nairobi from './public/nairobi.jpeg';
+import kisumu from './public/kisumu.jpeg';
+import mombasa from './public/mombasa.jpeg';
+import nakuru from './public/nakuru.jpeg';
+import meru from './public/meru.jpeg';
+import eldoret from './public/eldoret.jpeg';
+
+export const exploreData = [
+    {
+        img: nairobi,
+        location: 'Nairobi',
+    },
+    {
+        img: kisumu,
+        location: 'Kisumu',
+    },
+    {
+        img: mombasa,
+        location: 'Mombasa',
+    },
+    {
+        img: nakuru,
+        location: 'Nakuru',
+    },
+    {
+        img: eldoret,
+        location: 'Eldoret',
+    },
+    {
+        img: meru,
+        location: 'Meru',
+    }
+];
--- pages/index.js
+++ pages/index.js
@@ -1,8 +1,10 @@
-import Head from 'next/head'
-import Image from 'next/image'
-import Banner from '../components/Banner'
-import Header from '../components/Header'
-import styles from '../styles/Home.module.css'
+import Head from 'next/head';
+import Image from 'next/image';
+import Banner from '../components/Banner';
+import Header from '../components/Header';
+import styles from '../styles/Home.module.css';
+import { exploreData } from '../data';
+import SmallCard from '../components/SmallCard';
 
 export default function Home() {
   return (
@@ -15,6 +17,24 @@ export default function Home() {
 
       <Header /> 
       <Banner /> 
+
+      <main className='max-w-7xl mx-auto px-8 sm:px-16'>
+        <section className='pt-6'>
+          <h2 className='text-4xl font-semibold pb-5'>Explore Nearby</h2>
+          <div className="grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-3 xl:grid-cols-4">
+            {exploreData.map(({img, location}) => (
+              <SmallCard key={img} img={img} location={location} />
+            ))}
+          </div>
+        </section>
+      </main>
     </div>
   )
-}
+};
+
+// export async function getStaticProps() {
+//   const explore = fetch(exploreData).
+//     then((
+//       (res) 
+//     ))
+// }
